03653e852ac0a47e22309e7d6b54984b4e863ceb
Jared Mooring <jared.mooring@gmail.com>
2011-06-13 22:57:14 +1000

    * Allowed bestofborgy to reply back to tweets :$. Very buggy though

--- BestOfBorgnana/plugin.php
+++ BestOfBorgnana/plugin.php
@@ -15,6 +15,13 @@ require_once 'plugins/BestOfBorgnana/twitteroauth.php';
 
 class BestOfBorgnana extends SilverBotPlugin
 {
+    protected $_reply = true;
+
+    public function __construct()
+    {
+        $this->addTimer('checkMentions', '60 seconds', array($this, '_checkMentions'));
+    }
+
     public function chn_bestofborgy($data)
     {
         $data = $data['data'];
@@ -22,6 +29,7 @@ class BestOfBorgnana extends SilverBotPlugin
             $this->_tweetit($data);
         }
         elseif(!empty($this->_lastMessage)) {
+            $this->_reply = true;
             $this->_tweetit($this->_lastMessage);
         }
         else {
@@ -33,6 +41,11 @@ class BestOfBorgnana extends SilverBotPlugin
     {
         $id = $data['data'];
         $this->_deleteIt($id);
+    }
+
+    public function chn_checkmentions($data)
+    {
+        $this->_checkMentions();
     }
 
     public function onUserChannelMsg($data)
@@ -62,8 +75,11 @@ class BestOfBorgnana extends SilverBotPlugin
         $connection = $this->_getConnection();
         if($connection instanceof TwitterOAuth) {
             $content    = $connection->post("statuses/update", array('status' => $data));
-            if(isset($content->id_str) && !empty($content->id_str))
-                $this->bot->reply('@borgnana just tweeted: ' . $content->id_str . ' with: ' . $data);
+            if(isset($content->id_str) && !empty($content->id_str)) {
+                if($this->_reply)
+                    $this->bot->reply('@borgnana just tweeted: ' . $content->id_str . ' with: ' . $data);
+                $this->_lastMessage = '';
+            }
             else
                 $this->bot->reply("Can't tweet that. Something broke. Blame Dan Brown.");
         }
@@ -86,5 +102,37 @@ class BestOfBorgnana extends SilverBotPlugin
         catch(Exception $e) {
             return false;
         }
+    }
+
+    public function prv($data)
+    {
+        $username = $data['username'];
+        if($username == $this->config['username']) {
+            $text = $data['text'];
+            $this->_reply = false;
+            $this->_tweetIt('@' . $this->_replyTo . ' ' . $text);
+        }
+    }
+
+    protected function _checkMentions()
+    {
+        $connection = $this->_getConnection();
+
+        $lastmentionFile = $this->getDataDirectory('lastmention.txt');
+        $this->_lastId = file_get_contents($lastmentionFile);
+        $since = empty($this->_lastId) ? array() : array('since_id' => $this->_lastId);
+
+        $content = $connection->get('statuses/mentions', $since);
+        if(is_array($content)) {
+            foreach($content as $c) {
+                $this->_lastId  = $c->id;
+                $this->_replyTo = $c->user->screen_name;
+                $srch = array('@'. $this->config['username'], $this->config['username']);
+                $txt  = str_replace($srch, '', $c->text);
+                $this->bot->pm($this->config['username'], $txt);
+            }
+            file_put_contents($lastmentionFile, $this->_lastId);
+        }
+        return;
     }
 }
